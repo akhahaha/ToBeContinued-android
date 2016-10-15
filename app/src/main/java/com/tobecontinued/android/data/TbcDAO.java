f8f54740b5c1428f5c2ba67f998d6dd1c0cc6463
Alan Kha <akhahaha@gmail.com>
2016-10-15 09:26:16 -0700

    Add TbcDAO

---
+++ app/src/main/java/com/tobecontinued/android/data/TbcDAO.java
@@ -0,0 +1,33 @@
+package com.tobecontinued.android.data;
+
+import com.tobecontinued.android.model.Snippet;
+import com.tobecontinued.android.model.Story;
+import com.tobecontinued.android.model.User;
+
+import java8.util.concurrent.CompletableFuture;
+
+public interface TbcDAO {
+    CompletableFuture<User> createUser(User user);
+
+    CompletableFuture<User> updateUser(User user);
+
+    CompletableFuture<User> getUser(String userId);
+
+    CompletableFuture<Void> deleteUser(String userId);
+
+    CompletableFuture<Story> pushStory(Story story);
+
+    CompletableFuture<Story> updateStory(Story story);
+
+    CompletableFuture<Story> getStory(String storyId);
+
+    CompletableFuture<Void> deleteStory(String storyId);
+
+    CompletableFuture<Snippet> pushSnippet(Snippet snippet);
+
+    CompletableFuture<Snippet> updateSnippet(Snippet snippet);
+
+    CompletableFuture<Snippet> getSnippet(String snippetId);
+
+    CompletableFuture<Void> deleteSnippet(String snippetId);
+}
---
+++ app/src/main/java/com/tobecontinued/android/data/TbcDAOFirebombImpl.java
@@ -0,0 +1,81 @@
+package com.tobecontinued.android.data;
+
+import com.tobecontinued.android.model.Snippet;
+import com.tobecontinued.android.model.Story;
+import com.tobecontinued.android.model.User;
+
+import firebomb.Firebomb;
+import java8.util.concurrent.CompletableFuture;
+
+public class TbcDAOFirebombImpl implements TbcDAO {
+    private Firebomb firebomb;
+
+    public TbcDAOFirebombImpl(Firebomb firebomb) {
+        this.firebomb = firebomb;
+    }
+
+    @Override
+    public CompletableFuture<User> createUser(User user) {
+        return firebomb.persist(user);
+    }
+
+    @Override
+    public CompletableFuture<User> updateUser(User user) {
+        // TODO: Verify user exists
+        return firebomb.persist(user);
+    }
+
+    @Override
+    public CompletableFuture<User> getUser(String userId) {
+        return firebomb.find(User.class, userId);
+    }
+
+    @Override
+    public CompletableFuture<Void> deleteUser(String userId) {
+        return firebomb.remove(User.class, userId);
+    }
+
+    @Override
+    public CompletableFuture<Story> pushStory(Story story) {
+        story.setId(null);
+        return firebomb.persist(story);
+    }
+
+    @Override
+    public CompletableFuture<Story> updateStory(Story story) {
+        // TODO: Verify story exists
+        return firebomb.persist(story);
+    }
+
+    @Override
+    public CompletableFuture<Story> getStory(String storyId) {
+        return firebomb.find(Story.class, storyId);
+    }
+
+    @Override
+    public CompletableFuture<Void> deleteStory(String storyId) {
+        return firebomb.remove(Story.class, storyId);
+    }
+
+    @Override
+    public CompletableFuture<Snippet> pushSnippet(Snippet snippet) {
+        snippet.setId(null);
+        return firebomb.persist(snippet);
+    }
+
+    @Override
+    public CompletableFuture<Snippet> updateSnippet(Snippet snippet) {
+        // TODO: Verify snippet exists
+        return firebomb.persist(snippet);
+    }
+
+    @Override
+    public CompletableFuture<Snippet> getSnippet(String snippetId) {
+        return firebomb.find(Snippet.class, snippetId);
+    }
+
+    @Override
+    public CompletableFuture<Void> deleteSnippet(String snippetId) {
+        return firebomb.remove(Snippet.class, snippetId);
+    }
+}
